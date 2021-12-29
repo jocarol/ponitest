6c3364496fe15d29fd3ed56751e40e6fb242072f
joris.carol <joris.carol@moveecar.com>
2021-12-29 10:36:44 +0100

    Problem is solved but takes too much time to solve with large datasets

--- run.js
+++ run.js
@@ -46,5 +46,28 @@ rl.on('close', () => {
 });
 
 const solve = (L, C, N, queue) => {
-  console.log(`L: ${L}, C: ${C}, N: ${N}`);
+  // console.log(`L: ${L}, C: ${C}, N: ${N}`);
+  let sum = 0;
+  let total = 0;
+  //let seats = L;
+
+  console.time('for')
+  for (let i = 0; i < C; i++) {
+    let seats = 0;
+    console.log('ride', i);
+    for (let j = 0; j < N; j++) {
+      console.log(`Queue : ${queue}`);
+      console.log('current group size', queue[j])
+
+      if (queue[0] + seats <= L) {
+        console.log(`group ${j} of ${queue[0]} people gets on the ride n°${i}`);
+        sum += queue[0];
+        seats += queue[0];
+        console.log(`${L - seats} / ${L} seats remaining`)
+        queue.push(queue.shift())
+      }
+    }
+  }
+  console.timeEnd('for')
+  console.log('sum', sum);
 }
